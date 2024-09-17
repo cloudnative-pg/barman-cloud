a329d048b55cd53c5b29768a8d184bb6fa107c79
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2024-09-17 12:15:06 +0200

    fix: grammar
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/walarchive/doc.go
+++ pkg/walarchive/doc.go
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package walarchive implement the wal-archive command
+// Package walarchive implements the wal-archive command
 package walarchive
